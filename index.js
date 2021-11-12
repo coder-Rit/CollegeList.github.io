c385c75fc4d3709bb4ccee94f49e2ed2d0f8325f
coder-Rit <83896563+coder-Rit@users.noreply.github.com>
2021-11-13 00:42:33 +0530

    first commit

---
+++ index.js
@@ -0,0 +1,394 @@
+const MetaData=[
+    [
+        "6004 - Government College of Engineering & Research, Avasari Khurd",
+        9207,
+        93.5697957,
+       6363,
+       "CS" 
+    ],
+    [
+        "6006- College of Engineering, Pune",
+        188,
+        99.8670998,
+        ,
+       "CS" 
+    ],
+    [
+        "6122 - TSSMS's Pd. Vasantdada Patil Institute of Technology, Bavdhan, Pune",
+        28538,
+        80.0678416,
+        62053,
+       "CS" 
+    ],
+    [
+        "6138 - Genba Sopanrao Moze Trust Parvatibai Genba Moze College of Engineering,Wagholi, Pune",
+        52482,
+        58.7564176,
+        ,
+       "CS" 
+    ],
+    [
+        "6139 - Progressive Education Society's Modern College of Engineering, Pune",
+        15932,
+        "89.04/80.06",
+        ,
+       "IT/AI" 
+    ],
+    [
+        "6141 - Jaywant Shikshan Prasarak Mandal's,Rajarshi Shahu College of Engineering, Tathawade, Pune",
+        11618,
+        91.9284430,
+        ,
+       "IT" 
+    ],
+    [
+        "6144 - Genba Sopanrao Moze College of Engineering, Baner-Balewadi, Pune",
+        58854,
+        50.9892798,
+        ,
+       "CS" 
+    ],
+    [
+        "6145 - JSPM'S Jaywantrao Sawant College of Engineering,Pune",
+        28109,
+        80.3747784,
+        ,
+       "IT" 
+    ],
+    [
+        "6146 - MIT Academy of Engineering,Alandi, Pune",
+        7514,
+        94.6948134,
+        ,
+       "IT" 
+    ],
+    [
+        "6155 - G.H.Raisoni College of Engineering & Management, Wagholi, Pune",
+        23134,
+        83.9790664,
+        83000,
+       "IT" 
+    ],
+    [
+        "6156 - Marathwada Mitra Mandal's College of Engineering, Karvenagar, Pune",
+        19251,
+        86.7561408,
+        ,
+       "IT" 
+    ],
+    [
+        "6160 - JSPM's Imperial College of Engineering and Research, Wagholi, Pune",
+        26933,
+        81.2970469,
+        ,
+       "CS" 
+    ],
+    [
+        "6175 - Pimpri Chinchwad Education Trust, Pimpri Chinchwad College of Engineering, Pune",
+        3569,
+        97.4098856,
+        ,
+       "IT" 
+    ],
+    [
+        "6176 - G. H.Raisoni Institute of Engineering and Technology, Wagholi, Pune",
+        24004,
+        83.3493105,
+        ,
+       "IT" 
+    ],
+    [
+        "6177 - Sinhgad College of Engineering, Vadgaon (BK), Pune",
+        18803 ,
+        87.0496161,
+        ,
+       "IT" 
+    ],
+    [
+        "6178 - Sinhgad Technical Education Society's Smt. Kashibai Navale College of Engineering,Vadgaon,Pune",
+        27130,
+        81.1353574,
+        ,
+       "IT" 
+    ],
+    [
+        "6179 - Indira College of Engineering & Management, Pune",
+        33447,
+        76.1278833,
+        ,
+       "CS" 
+    ],
+    [
+        "6182 - Sinhgad Technical Education Society, Sinhgad Institute of Technology and Science, Narhe (Ambegaon)",
+        39430,
+        71.3623956,
+        ,
+       "IT" 
+    ],
+    [
+        "6184 - K. J.'s Educational Institut Trinity College of Engineering and Research, Pisoli, Haveli",
+        74943 ,
+        28.0907404,
+        ,
+       "IT" 
+    ],
+    [
+        "6185 - Sinhagad Institute of Technology, Lonavala",
+        53797 ,
+        57.0105054,
+        ,
+       "IT" 
+    ],
+    [
+        "6187 - Sinhgad Academy of Engineering, Kondhwa (BK) Kondhwa-Saswad Road, Pune",
+        44270,
+        66.7871101,
+        ,
+       "IT" 
+    ],
+    [
+        "6203 - Marathwada Mitra Mandal's Institute of Technology, Lohgaon, Pune",
+        40909 ,
+        69.9557928,
+        ,
+       "CS" 
+    ],
+    [
+        "6206 - Pune District Education Association's College of Engineering, Pune",
+        54769,
+        55.8259837,
+        ,
+       "IT" 
+    ],
+    [
+        "6207 - Dr. D. Y. Patil Vidya Pratishthan Society’s Dr. D. Y. Patil Institute of Technology, Pimpri, Pune",
+        16772,
+        "88.40/80.10",
+        ,
+       "IT/AI" 
+    ],
+    [
+        "6271 - Pune Institute of Computer Technology, Dhankavdi, Pune",
+        1741,
+        98.7246751,
+        ,
+       "IT" 
+    ],
+    [
+        "6272 - Dr. D. Y. Patil Pratishthan's D.Y.Patil College of Engineering Akurdi, Pune",
+        11225,
+        "92.23/85.43",
+        ,
+       "IT/AI" 
+    ],
+    [
+        "6273 - Bansilal Ramnath Agarawal Charitable Trust's Vishwakarma Institute of Technology, Bibwewadi, Pune",
+        1914,
+        98.6073116,
+        ,
+       "IT" 
+    ],
+    [
+        "6274 - Pune Vidyarthi Griha's College of Engineering and Technology and G K Pate(Wani) Institute of Management, Pune",
+        5721,
+        95.9637384,
+        ,
+       "IT" 
+    ],
+    [
+        "6275 - Shivnagar Vidya Prasarak Mandal's College of Engineering, Malegaon-Baramati",
+        50179,
+        61.2533921,
+        ,
+       "IT" 
+    ],
+
+    [
+        "6278 - All India Shri Shivaji Memorial Society's College of Engineering, Pune",
+        11025 ,
+        92.3440533 ,
+        ,
+       "CS" 
+    ],
+    [
+        "6281 - Modern Education Society's College of Engineering, Pune",
+        19045 ,
+        86.8357788,
+        ,
+       "CS" 
+    ],
+    [
+        "6282 - All India Shri Shivaji Memorial Society's Institute of Information Technology,Pune",
+        13552,
+        "90.66/80.65",
+        ,
+        "IT/AI" 
+    ],
+    [
+        "6284 - Vidya Pratishthan's Kamalnayan Bajaj Institute of Engineering & Technology, Baramati Dist.Pune",
+        34030,
+        "75.96/62.74",
+        ,
+       "IT/AI" 
+    ],
+    [
+        "6285 - Bharati Vidyapeeth's College of Engineering for Women, Katraj, Dhankawadi, Pune",
+        15501 ,
+        89.3221913,
+        ,
+       "IT" 
+    ],
+    [
+        "6289 - B.R.A.C.T's Vishwakarma Institute of Information Technology, Kondhwa (Bk.), Pune",
+        4930,
+        96.5092764,
+        ,
+       "IT" 
+    ],
+    [
+        "6298 - Zeal Education Society's Zeal College of Engineering & Reserch, Narhe, Pune",
+        54651,
+        "56.22/49.51",
+        ,
+       "IT/AI" 
+    ],
+    [
+        "6307 - Dhole Patil Education Society, Dhole Patil College of Engineering, Wagholi, Tal. Haveli",
+        51824,
+        59.3186035,
+        ,
+       "IT" 
+    ],
+    [
+        "6310 - Nutan Maharashtra Vidya Prasarak Mandal, Nutan Maharashtra Institute of Engineering &Technology, Talegaon station, Pune",
+        34796,
+        75.1527993,
+        ,
+       "IT" 
+    ],
+    [
+        "6311 - Jayawant Shikshan Prasarak Mandal, Bhivarabai Sawant Institute of Technology & Research, Wagholi ",
+        47562,
+        63.7042062,
+        ,
+         "IT" 
+    ],
+    [
+        "6419 - Nutan College of Engineering and Research, Talegaon Dabhade Tal. Maval, Pune",
+        56598 ,
+        53.6290323,
+        ,
+         "CSE(AI)" 
+    ],
+    [
+        "6649 - TSSM's Bhivarabai Sawant College of Engineering and Research, Narhe, Pune ",
+        43346,
+        68.0660974 ,
+        ,
+         "CS" 
+    ],
+    [
+        "6732 - Dr. D. Y. Patil School OF Engineering, Lohegaon, Pune",
+        35988 ,
+        74.3378698 ,
+        ,
+         "CS" 
+    ],
+    [
+        "6754 - International Institute of Information Technology (I²IT), Pune.",
+        9750,
+        93.2005060,
+        ,
+         "IT" 
+    ],
+    [
+        "6755 - JSPM Narhe Technical Campus, Pune.",
+        30693,
+        78.447316,
+        ,
+         "CS" 
+    ],
+    [
+        "6769 - Rasiklal M. Dhariwal Sinhgad Technical Institutes Campus, Warje, Pune.",
+        51315,
+        59.6157997,
+        ,
+         "CS" 
+    ],
+    [
+        "6772 - NBN Sinhgad Technical Institutes Campus, Pune",
+        47786,
+        63.4007679,
+        ,
+         "IT" 
+    ],
+    [
+        "6786 - Dr. D. Y. Patil School of Engineering & Technology, Charholi(Bk), Via Lohgaon, Pune",
+        43918 ,
+        66.9949148,
+        ,
+         "CS" 
+    ],
+    [
+        "6796 - Bharati Vidyapeeth's College of Engineering,Lavale, Pune",
+        17136 ,
+        88.1869064,
+        ,
+         "CS" 
+    ],
+    [
+        "6802 - Dr. D.Y.Patil Institute of Engineering, Management & Reseach, Akurdi, Pune",
+        15097 ,
+        "89.60/79.81",
+        ,
+         "CS/AI" 
+    ],
+    [
+        "6822 - Pimpri Chinchwad Education Trust's Pimpri Chinchwad College Of Engineering And Research, Ravet",
+        8082,
+        94.3436228,
+        ,
+         "CS" 
+    ],
+    [
+        "6834 - Dr.D.Y.Patil College Of Engineering & Innovation,Talegaon",
+        51910,
+        59.3186035 ,
+        ,
+         "CS" 
+    ]
+   
+    
+     
+     
+    
+]
+
+for (let i = 0; i < MetaData.length; i++) {
+    MetaData.sort((a,b)=>{
+         return a[1] - b[1]
+    })
+    const reversedData = MetaData
+    let html =''
+    reversedData.forEach((element, index)=>{
+        console.log(element);
+        html+=`<tr class="SingleData">
+        <td>${index+1}.</td>
+        <td class="clgName">${element[0]}</td>
+        <td class="Rank" >${element[4]}</td>
+        <td class="Rank" >${element[1]}</td>
+        <td class="AlineLeft" >${element[2]}%il</td>
+        <td class="Rank">${element[3]}/-</td>
+        </tr>
+         
+        `
+    })
+    let Meta_Data = document.getElementById('Meta_Data')
+    
+    Meta_Data.innerHTML=html
+    
+    
+
+}
+
+ 
